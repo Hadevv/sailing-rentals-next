adca1ee7fe6e680660f137d28c0166aa4c053fc7
Hadevv <ademeure29@gmail.com>
2024-01-28 18:29:06 +0100

    ✨ feature : create crud boat

---
+++ app/actions/action.ts
@@ -0,0 +1,46 @@
+import prisma from "@/lib/db";
+import { revalidatePath } from "next/cache";
+
+export async function createBoat(formData: FormData) {
+  "use server";
+
+  const input = formData.get("input") as string;
+
+  await prisma.boat.create({
+    data: {
+      input: input,
+    },
+  });
+
+  revalidatePath("/");
+}
+export async function updateBoat(formData: FormData) {
+  "use server";
+
+  const input = formData.get("input") as string;
+  const inputId = formData.get("inputId") as string;
+
+  await prisma.todo.update({
+    where: {
+      id: inputId,
+    },
+    data: {
+      input: input,
+    },
+  });
+
+  revalidatePath("/");
+}
+export async function deleteBoat(formData: FormData) {
+  "use server";
+
+  const inputId = formData.get("inputId") as string;
+
+  await prisma.todo.delete({
+    where: {
+      id: inputId,
+    },
+  });
+
+  revalidatePath("/");
+}
