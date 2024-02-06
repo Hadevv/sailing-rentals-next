b07f66633f455435db5969e79c078f0715405b56
Hadevv <ademeure29@gmail.com>
2024-02-06 20:18:10 +0100

    ✨ feature : add dynamic routing and display boat cards

--- app/boats/[id]/page.tsx
+++ app/boats/[id]/page.tsx
@@ -1,4 +1,36 @@
+import React from "react";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function getStaticPaths() {
+  const boats = await prisma.boat.findMany({
+    select: {
+      id: true,
+    },
+  });
+
+  const paths = boats.map((boat) => ({
+    params: { id: boat.id.toString() },
+  }));
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }: { params: { id: string } }) {
+  const boat = await prisma.boat.findUnique({
+    where: {
+      id: Number(params.id),
+    },
+  });
+
+  return { props: { boat } };
+}
+
 export default function Page({ params }: { params: { id: number } }) {
-    return <div>My Post ID: {params.id}</div>;
-  }
-  
+  return (
+    <div>
+      <h1>{params.id}</h1>
+    </div>
+  );
+}
--- app/boats/page.tsx
+++ app/boats/page.tsx
@@ -1,2 +1,60 @@
+import React from "react";
+import prisma from "../../lib/db";
+import Image from "next/image";
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import ulyc from "../public/img/ulyc.jpeg";
 
+async function getBoat() {
+  "use server";
+  const boats = await prisma.boat.findMany({
+    select: {
+      id: true,
+      name: true,
+      type: true,
+      description: false,
+      imageUrl: true,
+      ownerId: true,
+      owner: true,
+    },
+  });
 
+  return boats;
+}
+
+export default async function Home() {
+  const boats = await getBoat();
+  console.log(boats);
+
+  return (
+    <div>
+      <div className="flex justify-center gap-5">
+        {boats.map((boat) => (
+          <Card className="w-64" key={boat.id}>
+            <CardHeader>
+              <CardTitle>{boat.name}</CardTitle>
+              <CardDescription>{boat.type}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Image
+                src={ulyc}
+                alt={boat.name}
+                width={300}
+                height={200}
+              />
+            </CardContent>
+            <CardFooter>
+                <CardDescription>{boat.owner.username}</CardDescription>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
