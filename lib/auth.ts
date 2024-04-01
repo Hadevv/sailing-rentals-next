aa169b227b175bc202ddd0d6d8e5a4611531d34a
Hadevv <ademeure29@gmail.com>
2024-04-01 21:29:20 +0200

    🔒security : NextAuthConfig update authentication configuration and add getCurrentUser

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,25 +1,60 @@
-import NextAuth from 'next-auth';
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import prisma from './db';
-import GithubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { type User as PrismaUser } from '@prisma/client'
+import type { NextAuthConfig } from 'next-auth'
+import NextAuth from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
 
-export const { auth, handlers } = NextAuth({
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GithubProvider({
-        clientId: process.env.GITHUB_ID,
-        clientSecret: process.env.GITHUB_SECRET,
-        }),
-    ],
-    secret: process.env.SECRET,
-    session: {
-        jwt: true,
-    },
-    callbacks: {
-        async session(session, user) {
-        session.id = user.id;
-        return Promise.resolve(session);
-        },
-    },
-    });
-    
+
+import prisma from './db'
+
+declare module 'next-auth' {
+  // We're extending interfaces from 'next-auth' and we can't use 'type' for these declarations.
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  interface Session {
+    user: PrismaUser
+  }
+
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions, @typescript-eslint/no-empty-interface
+  interface User extends PrismaUser {}
+}
+
+const config: NextAuthConfig = {
+  secret: env.NEXTAUTH_SECRET,
+  providers: [
+    GithubProvider({
+      clientId: env.OAUTH_CLIENT_KEY,
+      clientSecret: env.OAUTH_CLIENT_SECRET
+    }),
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET
+    })
+  ],
+
+  adapter: PrismaAdapter(prisma),
+
+  callbacks: {
+    session: ({ session, user }) => {
+      session.user.id = user.id
+      session.user.role = user.role
+
+      return session
+    }
+  }
+}
+
+export const {
+  handlers: { GET, POST },
+  auth
+} = NextAuth(config)
+
+export const getCurrentUser = async () => {
+  const session = await auth()
+
+  if (!session?.user) {
+    return null
+  }
+
+  return session.user
+}
